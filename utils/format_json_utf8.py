977eabfb4afcde023527787a37d5977e46d0ff81
Jérémy Talbot-Pâquet <jtpaquet@gmail.com>
2019-10-09 21:32:54 -0400

    organisation

---
+++ utils/format_json_utf8.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+src_filename = "unicode_touka.json"
+dst_filename = "utf_touka.json"
+
+with open(src_filename, 'r') as src_file:
+    with open(dst_filename, 'w+') as dst_file:
+        for line in src_file:
+            dst_file.write(line.encode('utf8').decode('utf8'))
+
+u1 = "J\u00c3\u00a9r\u00c3\u00a9my Talbot-P\u00c3\u00a2quet"
+u2= "J\u00c3\u00a9r\u00c3\u00b4me Sirois Charron"
+print(u1.encode('latin1').decode('utf-8'))
+print(u2.encode('latin1').decode('utf-8'))
