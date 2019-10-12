31cc3736db30e138200e34fae98509a83e6af955
Jérémy Talbot-Pâquet <jtpaquet@gmail.com>
2019-10-12 18:35:13 -0400

    Add proper labeling GUI setup

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,18 +1,17 @@
+import os
 from pymongo import MongoClient
-from GUI import Gui, Gui_labelling
+from GUI import Gui_labelling
 import tkinter as tk
 
-
 if __name__ == '__main__':
+
+    file_path = os.path.dirname(os.path.realpath(__file__))
 
     # client = MongoClient("localhost", 27017) # When offline
     client = MongoClient("mongodb+srv://jtpaquet:pv9E9SB5gAVzKWbW@toukaanalytics-epm7v.gcp.mongodb.net/ToukaAnalytics?retryWrites=true&w=majority")
     db = client['ToukaAnalytics']
-    #root = tk.Tk()
     root_labelling = tk.Tk()
     root_labelling.title('Labelling Data for Chatbot')
-    #db_manager = Gui(root, db)
+    root_labelling.iconbitmap(os.path.join(file_path, 'favicon.ico'))
     training_window = Gui_labelling(root_labelling, db)
-    # root.protocol("WM_DELETE_WINDOW", db_manager.kill_running_processes())
-    #root.mainloop()
     root_labelling.mainloop()
