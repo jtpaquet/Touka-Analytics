6f35a452848d1e366ff19a36d1afefea8c33d2e6
Jérémy Talbot-Pâquet <jtpaquet@gmail.com>
2019-11-13 23:17:34 -0500

    requirements and list authors and att

--- utils/list.py
+++ utils/list.py
@@ -3,15 +3,19 @@ import codecs
 from datetime import datetime
 from mongoengine import connect, Document
 
-list= []
+msg_att= []
+authors = []
 
-data = json.load(codecs.open('touka.json', 'r', 'utf-8')
-                 )  # Pour avoir les caractères spéciaux
+data = json.load(codecs.open('touka.json', 'r', 'utf-8'))  # Pour avoir les caractères spéciaux
 
 for message in data['messages']:
     for att in message.keys():
-        if att not in list:
-            list.append(att)
+        if att not in msg_att:
+            msg_att.append(att)
+    
+    if message['sender_name'] not in authors:
+        authors.append(message['sender_name'])
 
-print(list)
+print(msg_att)
+print(authors)
 print(len(data['messages']))
