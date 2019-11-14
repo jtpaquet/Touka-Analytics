b511fe6de6f8aad9b376f8e445ece53d1a2cdc51
jtpaquet <jtpaquet@gmail.com>
2019-11-14 13:51:15 -0500

    update app.py to make it faster

--- app/app.py
+++ app/app.py
@@ -13,8 +13,7 @@ MONGODB_PORT = 27017
 MONGODB_URI = "mongodb+srv://jtpaquet:pv9E9SB5gAVzKWbW@toukaanalytics-epm7v.gcp.mongodb.net/ToukaAnalytics?retryWrites=true&w=majority"
 DBS_NAME = 'ToukaAnalytics'
 FIELDS = {'content': True, 'author' : True, 'timestamp' : True, 'type' : True}
-connection = MongoClient(MONGODB_URI)
-database = connection[DBS_NAME]
+
 
 @app.route("/")
 def index():
@@ -22,59 +21,56 @@ def index():
 
 @app.route("/rawDB")
 def rawDB():
+	connection = MongoClient(MONGODB_URI)
+	database = connection[DBS_NAME]
 	collection = database['messages']
 	projects = collection.find(projection=FIELDS, limit=5000)
 	json_projects = [project for project in projects]
 	json_projects = json.dumps(json_projects, default=json_util.default)
+	connection.close()
 	return json_projects
 
 	
 @app.route("/ToukaAnalytics")
 def ToukaAnalytics():
+	connection = MongoClient(MONGODB_URI)
+	database = connection[DBS_NAME]
 	members = database['members']
 	messages = database['messages']
 	
 	data = []
 	for member in members.find():
 		name = member['name']
-		# messages.aggregate( [ { "$project" : { 'content' : 1 , 'date' : 1 } } ] )
 		query_txt = {'$and': [ {'author': name}, {'content': {'$exists': True, '$ne': None } } ] }
 		query_react = {'$and': [ {'author': name}, {'reaction': {'$exists': True, '$ne': None } } ] }
-
 		member_messages, member_timestamps, member_types, member_reacts = [], [], [], []
+		fields = {'content': True, 'timestamp': True, 'type': True, 'reactions': True, '_id': False}
+        
+		fetched_data = list(messages.find(query_txt, fields))
+		
+		member_data = {}
+		for field in list(fields.keys())[:-1]:
+			member_data[field] = list(map(lambda d: d[field], fetched_data))
+			if field== 'content':
+				s = 0
+				for msg in member_data[field]:
+					s += len(msg)
+		
+		member_data['n_char'] = s
+		member_data['n_msg'] = len(member_data['content'])
+		member_data['ratio_char_msg'] = member_data['n_char'] / member_data['n_msg']
+		member_data['name'] = member['pseudo']
 
-		for msg in messages.find(query_txt): # Changer ça ici, c'est long af
-			member_messages.append(msg['content'])
-			member_timestamps.append(msg['timestamp'])
-			member_types.append(msg['type'])
-			member_reacts.append(msg['reactions'])
+		data.append(member_data)
 
-		n_msg = messages.find(query_txt).count()
+	df = pd.DataFrame(data, columns=('name', 'content', 'timestamp', 'type', 'reactions', 'n_msg', 'n_char', 'ratio_char_msg'))
+	df = df.set_index('name')
 
-		data.append( {'Name': member['pseudo'], 'txt_msg': member_messages, 'msg_timestamps' : member_timestamps, 'Type': member_types, 'Reaction' : member_reacts, 'msg_count' : n_msg} )
-
-	df = pd.DataFrame(data, columns=('Name', 'txt_msg', 'msg_timestamps', 'Type', 'Reaction', 'msg_count'))
-	df = df.set_index('Name')
-
-	nb_characters = []
-	nb_characters_total = []
-	for member in df.index:
-		s = 0
-		s_total = []
-		for msg in df.loc[member, 'txt_msg']:
-			s += len(msg)
-			s_total.append(s)
-		nb_characters.append(s_total)
-		nb_characters_total.append(s)
-
-	df['char_count'] = nb_characters_total
-
-	df['ratio_char_msg'] = df['char_count'] / df['msg_count']
-
-	df = df.drop(columns=["txt_msg", "Type", "Reaction"])
+	df = df.drop(columns=["content", "type", "reactions"])
 	
-	df_json = json.dumps(df.to_json(), default=json_util.default)
-	return df.to_json()
+	df_json = json.dumps(df.to_html(), default=json_util.default)
+	connection.close()
+	return json.dumps(df_json)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
