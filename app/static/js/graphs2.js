d6128a62325f992536aa48320e8a4cc942ee374b
jtpaquet <jtpaquet@gmail.com>
2019-11-15 17:29:34 -0500

    Update graphs2.js

--- app/static/js/graphs2.js
+++ app/static/js/graphs2.js
@@ -11,9 +11,11 @@ function makeGraphs(error, projectsJson, membersJson) {
 	membersJson.forEach(function(d) {
 		membersData[d['name']] = d['pseudo'];
 	});
-	var dateFormat = d3.time.format("%Y-%m-%d");
+	var dateFormat = d3.time.format("%Y-%m");
+	var timeFormat = d3.time.format("%a %d %b %Y à %Hh%Mm%ss");
 	toukaData.forEach(function(d) {
 		d["timestamp"] = new Date(d['timestamp']);
+		d["month"] = dateFormat(new Date(d['timestamp']));
 		d['author'] = membersData[d['author']];
 	});
 
@@ -21,17 +23,22 @@ function makeGraphs(error, projectsJson, membersJson) {
 	var ndx = crossfilter(toukaData);
 
 	//Define Dimensions
-	var authorDim = ndx.dimension(function(d) { return d["author"] });
+	var authorDim = ndx.dimension(function(d) { return d.author });
 	// Si je veux avoir tous les messages écrits par un auteur, je fais authorDim.filter('author')
-	var dateDim = ndx.dimension(function(d) { return d["timestamp"] });
-	var typeDim = ndx.dimension(function(d) { return d["type"] });
-	var contentDim = ndx.dimension(function(d) { return d["content"] });
+	var dateDim = ndx.dimension(function(d) { return d.timestamp });
+	var monthDim = ndx.dimension(function(d) { return d.month });
+	var typeDim = ndx.dimension(function(d) { return d.type });
+	// var contentDim = ndx.dimension(function(d) { return d.content }); // On en a pas vrm besoin
 
 	//Calculate metrics
 	var msgByAuthor = authorDim.group();
-	var msgByDate = dateDim.group(); 
+	var n_msgByAuthor = msgByAuthor.top(msgByAuthor.size());
+	// msgByAuthor.reduceCount().all()
+	// var msgByDate = dateDim.group(); // pas besoin
+	var msgByMonth = monthDim.group();
 	var msgByType = typeDim.group();
-	var msgByContent = contentDim.group();
+	// msgByType.reduceCount().all()
+	// var msgByContent = contentDim.group(); // pas besoin
 
 	var all = ndx.groupAll();
 	var totalMsg = all.reduceCount().value();
@@ -39,37 +46,44 @@ function makeGraphs(error, projectsJson, membersJson) {
 	//Define values (to be used in charts)
 	var minDate = dateDim.bottom(1)[0]["timestamp"];
 	var maxDate = dateDim.top(1)[0]["timestamp"];
+	var minMonth = monthDim.bottom(1)[0]["month"];
+	var maxMonth = monthDim.top(1)[0]["month"];
 
     //Charts
-	var timeChart = dc.lineChart("#time-chart");
-	var msgCountChart = dc.barChart("#msg-count-bar-chart");
+	// var timeChart = dc.lineChart("#time-chart");
+	var msgCountChart = dc.barChart("#msg-count-row-chart");
 	var totalMsgND = dc.numberDisplay("#total-msg-nd");
-
-	totalMsgND
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(totalMsg)
-		.formatNumber(d3.format(".3s"));
-
-	timeChart
-		.width(600)
-		.height(160)
-		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(dateDim)
-		.group(msgByDate)
-		.transitionDuration(500)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.elasticY(true)
-		.xAxisLabel("Year")
-		.yAxis().ticks(4);
+	var toukaCreation = timeFormat(minDate);
 
 	msgCountChart
         .width(300)
-        .height(250)
+        .height(280)
+		.x(d3.scale.ordinal().domain(n_msgByAuthor.map(function(d) {return d.key })))
+		.y(d3.scale.linear().domain([0, 55000]))
+		.xUnits(dc.units.ordinal)
+		.brushOn(false)
+	    .yAxisLabel("Messages envoyés")
         .dimension(authorDim)
-        .group(msgByAuthor)
-        .xAxis().ticks(4);
+		.group(msgByAuthor)
+		
+	totalMsgND
+		.formatNumber(d3.format("d"))
+		.valueAccessor(function(d){return d; })
+		.group(all)
+		.formatNumber(d3.format(".3s"));
+
+	// timeChart
+	// 	.width(600)
+	// 	.height(160)
+	// 	.margins({top: 10, right: 50, bottom: 30, left: 50})
+	// 	.dimension(dateDim)
+	// 	.group(msgByDate)
+	// 	.transitionDuration(500)
+	// 	.x(d3.time.scale().domain([minDate, maxDate]))
+	// 	.elasticY(true)
+	// 	.xAxisLabel("Year")
+	// 	.yAxis().ticks(4);
 
     dc.renderAll();
-
+	console.log("--Graphics rendered--")
 };
