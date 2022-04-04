8a9048b682c6bc3f832784ff0c3264561524d683
unknown <jtpaquet@gmail.com>
2022-04-04 02:08:25 -0400

    updated db

--- utils/json_merge.py
+++ utils/json_merge.py
@@ -2,7 +2,7 @@ import os
 import json
 import codecs
 
-files = [os.path.join("touka_08dec2021", f"message_{i+1}.json") for i in range(23)]
+files = [os.path.join("touka_04avril2022", f"message_{i+1}.json") for i in range(24)]
 all_data = []
 
 for file in files:
@@ -13,6 +13,6 @@ for file in files:
 all_data = [item for sublist in all_data for item in sublist]
 
 
-with open(os.path.join("touka_08dec2021", "touka_08dec2021.json"), "w") as json_file:
+with open(os.path.join("touka_04avril2022", "touka_04avril2022.json"), "w") as json_file:
     json.dump(all_data, json_file, indent=4, sort_keys=True)
 
