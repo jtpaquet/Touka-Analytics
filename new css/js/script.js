19cffdf1231f7e31f00c0cd72a552dbc1521c346
jtpaquet <jtpaquet@gmail.com>
2022-11-28 18:46:33 -0500

    New website layout

---
+++ new css/js/script.js
@@ -0,0 +1,30 @@
+const sidebar = document.querySelector('.sidebar');
+const navItems = document.querySelectorAll('nav .nav-item');
+const toggle = document.querySelector('.sidebar .toggle');
+
+console.log("Wesh!");
+
+toggle.addEventListener('click', () => {
+
+    if (sidebar.className == "sidebar") {
+        sidebar.classList.add('open');
+    }
+    else {
+        sidebar.classList.remove('open');
+    }
+
+});
+
+navItems.forEach(navItem => {
+
+    navItem.addEventListener('click', () => {
+
+        navItems.forEach(navItem => {
+            navItem.classList.remove('active')
+        });
+
+        navItem.classList.add('active');
+
+    });
+
+});
